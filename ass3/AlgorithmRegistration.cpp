ee5d0a179b1523d2819c8ff378e6a6d93657585a
xqgex <xqgex.n@gmail.com>
2018-07-22 11:58:26 +0300

    Initial commit

---
+++ ass3/AlgorithmRegistration.cpp
@@ -0,0 +1,20 @@
+/*
+ * AlgorithmRegistration.cpp
+ *
+ *      Course: Advanced Topics In Programming
+ *    Exercise: 2
+ *      Author: Ilor Ifrach & Yogev Neumann
+ * Description: Register PlayerAlgorithm implementation
+ */
+
+#include <functional>
+#include <memory>
+#include <iostream>
+#include <map>
+
+#include "AlgorithmRegistration.h"
+#include "TournamentManager.h"
+
+AlgorithmRegistration::AlgorithmRegistration(std::string id, std::function<std::unique_ptr<PlayerAlgorithm>()> factoryMethod) {
+    TournamentManager::getTournamentManager().registerAlgorithm(id, factoryMethod);
+}
